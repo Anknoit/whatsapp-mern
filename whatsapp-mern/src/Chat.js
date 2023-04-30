2a4607ff04b5c8bcd50495e0c2270fde31f549fa
Anknoit <ankitjha8891@gmail.com>
2023-05-01 00:48:01 +0530

    built chat pannel

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,9 +1,33 @@
 import React from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@mui/material";
+import {
+  AttachFile,
+  MoreVertOutlined,
+  SearchOutlined,
+} from "@mui/icons-material";
 function Chat() {
   return (
     <div className="chat">
-      <h1>Chat Pannel</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen...</p>
+        </div>
+        <div className="chat__headerRight">
+          {/* <IconButton></IconButton> gives click functionality to the button */}
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertOutlined />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
